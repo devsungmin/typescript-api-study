052ea52f4243d7f8b6034431bde4152b2cd78545
devsungmin <kimsungmin.dev@gmail.com>
2021-06-10 23:55:13 +0900

    Fix: docker db setting

--- typescript_server/src/config/config.ts
+++ typescript_server/src/config/config.ts
@@ -3,11 +3,11 @@ dotenv.config();
 
 export const config = {
     development: {
-        username: process.env.DB_USERNAME || 'root',
+        username: process.env.DB_USERNAME || 'typescript_server_db',
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_DBNAME || 'typescript_test',
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || 3306,
-        dialect: "mysql"
+        database: process.env.DB_DBNAME || 'typescript_api_study',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT
     }
 }
