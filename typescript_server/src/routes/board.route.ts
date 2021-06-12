fdbeaf5e87463611ea888c88710f20890ad0a0e5
devsungmin <kimsungmin.dev@gmail.com>
2021-06-12 16:21:44 +0900

    Fix: board route

--- typescript_server/src/routes/board.route.ts
+++ typescript_server/src/routes/board.route.ts
@@ -5,7 +5,7 @@ const router = express.Router()
 const ctrl = new boardController
 
 router.route('/').get(ctrl.getAllBoard);
-router.route('/test').get(ctrl.getBoard);
+router.route('/:id').get(ctrl.getBoard);
 
 router.route('/').post(ctrl.createBoard);
 
