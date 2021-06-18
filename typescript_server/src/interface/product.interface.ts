3df688f6901fb2af36b607e8c900a5b47a439931
devsungmin <kimsungmin.dev@gmail.com>
2021-06-18 23:20:33 +0900

    Add: product Model

---
+++ typescript_server/src/interface/product.interface.ts
@@ -0,0 +1,9 @@
+interface Product {
+    id: number,
+    name: string,
+    price: number,
+    category: string,
+    sellerId: number,
+    salePrice: number
+}
+export default Product;
---
+++ typescript_server/src/models/Product.model.ts
@@ -0,0 +1,50 @@
+import { Model, DataTypes, Association, BuildOptions, Sequelize } from 'sequelize';
+import productInterface from '../interface/product.interface';
+import { Seller } from './Seller.model';
+
+export class Product extends Model<productInterface> {
+    public id?: number;
+    public name!: string;
+    public price!: number;
+    public category!: string;
+    public sellerId!: number;
+    public salePrice!: number;
+
+    public static associations: {
+        productHasManyScores: Association<Product>;
+    }
+}
+
+export type ProductStatic = typeof Model & {
+    new(values?: object, options?: BuildOptions): Product
+}
+
+export function ProductFactor(sequelize: Sequelize): ProductStatic {
+    return <ProductStatic>sequelize.define("product", {
+        id: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.NUMBER,
+            allowNull: false
+        },
+        category: {
+            type: DataTypes.STRING(30),
+            allowNull: false
+        },
+        sellerId: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+        },
+        salePrice: {
+            type: DataTypes.NUMBER,
+            allowNull: true
+        }
+    })
+}
